711492de87cc566cd204f9c09a11ab9afd1d978a
AATataj <muresan44@gmail.com>
2020-10-06 16:09:01 -0400

    have a really basic post request recieving/sending

--- nba_back/backend/views.py
+++ nba_back/backend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
+@csrf_exempt
 def dbQuery (request):
-    return HttpResponse("hellows")
+    print("we're in the dbQuery function...")
+    print(json.load(request))
+    histatus = { "hiStatus" : "hellows" }
+    histatusJson = json.dumps(histatus)
+    return JsonResponse(histatusJson, safe=False)
