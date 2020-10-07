f6f442c0ff405d29ed412bf35f5cb7d32a05fb97
AATataj <muresan44@gmail.com>
2020-10-07 19:01:00 -0400

    have a simple query working fetching data from db and returning in json format

--- nba_back/backend/views.py
+++ nba_back/backend/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
+from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from backend.models import Boxscores
 import json
+
 # Create your views here.
 
 @csrf_exempt
 def dbQuery (request):
-    print("we're in the dbQuery function...")
-    print(json.load(request))
-    histatus = { "hiStatus" : "hellows" }
-    histatusJson = json.dumps(histatus)
-    return JsonResponse(histatusJson, safe=False)
+    queryData = json.load(request)
+    print(queryData.get('name'))
+    print(queryData.get('startDate'))
+    print(queryData.get('endDate'))
+    print(queryData.get('playerID'))
+
+    queryResults = Boxscores.objects.filter(name = queryData.get('name'),
+                                            date__lte= queryData.get('endDate'), 
+                                            date__gte= queryData.get('startDate'),
+                                            )[:5]
+
+
+    print (str(len(queryResults)))
+    print(queryResults[0].name + " " + str(queryResults[0].pts) + " " + str(queryResults[0].date))
+    print(queryResults[1].name + " " + str(queryResults[1].pts) + " " + str(queryResults[1].date))
+    jsonReply = serializers.serialize("json", queryResults)
+    return JsonResponse(jsonReply, safe=False)
+    # histatus = { "hiStatus" : "hellows" }
+    # histatusJson = json.dumps(histatus)
+    # return JsonResponse(histatusJson, safe=False)
