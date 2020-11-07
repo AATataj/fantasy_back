7b2271163c50a0d83bc44dd8acb8cf47227c6cf4
AATataj <muresan44@gmail.com>
2020-11-06 20:46:44 -0500

    solved the channels cors issue, working on handling requests now

--- nba_back/backend/views.py
+++ nba_back/backend/views.py
@@ -3,22 +3,7 @@ from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from backend.models import Boxscores
-import socketio
-import json, os
-
-async_mode = None
-basedir = os.path.dirname(os.path.realpath(__file__))
-sio = socketio.Server(async_mode='eventlet')\
-@sio.on('connection-bind')
-def connection_bind(sid, data):
-    ## code to capture data
-    ## sid is unique for each connection and data is the additional
-    ## info for the connection (roto, box, plays etc)
-
-@sio.on('disconnect')
-def test_disconnect(sid):
-    ## code to capture data
-    ## etc etc
+import json
 
 # Create your views here.
 @csrf_exempt
@@ -41,6 +26,12 @@ def dbQuery (request):
     print(queryResults[1].name + " " + str(queryResults[1].pts) + " " + str(queryResults[1].date))
     jsonReply = serializers.serialize("json", queryResults)
     return JsonResponse(jsonReply, safe=False)
-    # histatus = { "hiStatus" : "hellows" }
-    # histatusJson = json.dumps(histatus)
-    # return JsonResponse(histatusJson, safe=False)
+
+def Scraperoto(request):
+    ## in here we'll need the function to call the scraper
+    ## functions and get periodic results to push back to the front end
+
+    # for now, let's just handle the request for the websocket and 
+    # make sure it works with only a timer
+    print (request)
+    return
--- nba_back/nba_back/asgi.py
+++ nba_back/nba_back/asgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+##### not original
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+import nba_back.routing
+#### /not original
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nba_back.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+##### not original 
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+    # Just HTTP for now. (We can add other protocols later.)
+})
+#### /not original
