3060c47f5e8f15b00de1c4e8a572e81dde14ef67
AATataj <muresan44@gmail.com>
2020-11-12 00:14:10 -0500

    working on the routing for the other scrapers

--- nba_back/nba_back/consumers.py
+++ nba_back/nba_back/consumers.py
@@ -4,32 +4,53 @@ import mysql.connector
 
 from .externalMsgs import testingExternal
 sys.path.insert(1, sys.path[0]+'/../../fantasy_bot')
-from fantasyAI import rotoworldScraper
+from fantasyAI import rotoworldScraper, liveBoxScrape # scraper for play-by-plays to be added
 
-
-class ProgressConsumer(WebsocketConsumer):
+class ProgressRoto(WebsocketConsumer):
     def connect(self):
-        print("do we ever get into the connect function?")
-        self.accept()
-        
+        self.accept()        
         cnx = mysql.connector.connect(user="slick", password = "muresan44", host ='127.0.0.1', database='nba')
-
         rotoworldScraper.updateRoto(cnx, self)
-
         self.send(text_data=json.dumps({
         'progress': 100
         }))
-        
         self.close()
-
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json['message']
-        # self.send(text_data=json.dumps({
-        #     'message': message
-        # }))
-        print ("message received")
+        "this is in case we want to recieve more data from the front end"
+class ProgressBox(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        cnx = mysql.connector.connect(user="slick", password = "muresan44", host ='127.0.0.1', database='nba')
+        liveBoxScrape.scrapeScores(cnx, self)
+        self.send(text_data=json.dumps({
+        'progress': 100
+        }))
+        self.close()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        "this is in case we want to recieve more data from the front end" 
+class ProgressPlay(WebsocketConsumer):
+    """
+    We'll flesh this out after we get a scraper for play by plays
+    """
+    # def connect(self):
+    #     self.accept()
+    #     cnx = mysql.connector.connect(user="slick", password = "muresan44", host ='127.0.0.1', database='nba')
+    #     liveBoxScrape.scrapeScores(cnx, self)
+    #     self.send(text_data=json.dumps({
+    #     'progress': 100
+    #     }))
+    #     self.close()
+
+    # def disconnect(self, close_code):
+    #     pass
+
+    # def receive(self, text_data):
+    #     "this is in case we want to recieve more data from the front end" 
--- nba_back/nba_back/routing.py
+++ nba_back/nba_back/routing.py
@@ -4,8 +4,10 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ScrapeAll/', consumers.ProgressConsumer.as_asgi()),
-    path('ScrapeRoto/', consumers.ProgressConsumer.as_asgi()),
-    path('ScrapeBox/', consumers.ProgressConsumer.as_asgi()),
-    path('ScrapePlays/', consumers.ProgressConsumer.as_asgi()),
+    ## I don't think I need a landing spot for scrapeAll as the sockets will 
+    ## be requested individually from front end
+    # path('ScrapeAll/', consumers.ProgressAll.as_asgi()),
+    path('ScrapeRoto/', consumers.ProgressRoto.as_asgi()),
+    path('ScrapeBox/', consumers.ProgressBox.as_asgi()),
+    path('ScrapePlays/', consumers.ProgressPlay.as_asgi()),
 ]
